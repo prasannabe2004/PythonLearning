0d1b56667d580c9b0fd94d41aec7e7702e3ce9e8
prasannabe2004 <prasannabe2004@gmail.com>
2014-05-19 18:17:47 -0500

    Launching website with movies link

--- media.py
+++ media.py
@@ -1,6 +1,7 @@
 import webbrowser
 
 class Movie():
+    """ Classs for movies store """
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
