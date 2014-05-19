6c3dec9dd16273d3f0d37286da83bf7ded001bd6
prasannabe2004 <prasannabe2004@gmail.com>
2014-05-19 17:29:35 -0500

    Test Class Python
    From udacity, this class explain the movies store with class
    implementation.

---
+++ entertainment.py
@@ -0,0 +1,10 @@
+import media
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=vwyZH85NQC4")
+print(toy_story.title)
+
+#avatar = media.Movie("Avatar", "A marine on an alien planet", "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=-9ceBgWV8io")
+#avatar.show_trailer()
+
+thupakki = media.Movie("Thuppaki", "The story revolves around an Indian army intelligence officer from a Mumbai-based Tamil family", "http://en.wikipedia.org/wiki/File:First_look_Thuppakki.jpg", "https://www.youtube.com/watch?v=aW_j4pNvG98")
+print(thupakki.storyline)
+thupakki.show_trailer()
