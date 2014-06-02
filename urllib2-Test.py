a029652e8f9cf91b6ecb1cb38d030bdc2d32e1ba
prasannabe2004 <prasannabe2004@gmail.com>
2014-06-02 16:26:05 -0500

    URL Lib test in python with HTTP req/res and POST requests.

---
+++ urllib2-Test.py
@@ -0,0 +1,29 @@
+# This works with Python 2.7.6 Version
+# Interesting website to test HTTP req/res and POST: http://httpbin.org/
+
+import urllib2
+import urllib
+
+## that's the GET request
+res = urllib2.urlopen('http://httpbin.org/ip')
+
+print res.read()
+
+## post requests contain data, so here's how to make a POST request
+params = {}
+
+## set post parameters
+params['article_id'] = 134
+params['text_str'] = 'my_test_program'
+
+## encode the data to percent format
+data = urllib.urlencode(params)
+
+req = urllib2.Request('http://httpbin.org/post',data)
+
+## make request
+res = urllib2.urlopen(req)
+
+## print output
+print res.read()
+
