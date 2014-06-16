edd439271363d72c212695e2c0e437a52c7a7491
prasannabe2004 <prasannabe2004@gmail.com>
2014-06-16 10:16:12 -0500

    Adding update in project

--- udacity/Project/employee.py
+++ udacity/Project/employee.py
@@ -1,13 +1,20 @@
 class Employee():
-    
     def __init__(self,firstName, lastName, employeeNum, age):
-        self.firstName = firstName
+        self.__firstName = firstName
         self.lastName = lastName
         self.employeeNum = employeeNum
         self.age = age
+        print("Welcome " + self.__firstName + " " + self.lastName)
         
     def getInfo(self):
-        print(self.firstName+self.lastName+" "+str(self.employeeNum)+" "+str(self.age))
+        print(self.__firstName+" "+self.lastName+" "+str(self.employeeNum)+" "+str(self.age))
+    def __setAge(age):
+    		b n.  age = age
+    def setInfo(self, age):
+     	__setAge(age)
+
+
+
         
 #class FullTime(Employee):
 #   def __init__():
--- udacity/Project/main.py
+++ udacity/Project/main.py
@@ -1,5 +1,14 @@
 from employee import Employee
 
-emp = Employee("Prasanna", "Mohanasundaram",21870,30)
-emp.getInfo()
+print("Invoke using object")
+emp1 = Employee("Prasanna", "Mohanasundaram",21870,30)
+emp1.getInfo()
+
+emp2 = Employee("FIRSTNAME","LASTNAME",11223,99)
+emp2.getInfo()
+
+emp2.setInfo(55)
+
+emp2.getInfo()
+
 
