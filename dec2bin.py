b06067a2878822cfba97e1f75cc21c09ec492ef2
prasannabe2004 <prasannabe2004@gmail.com>
2014-07-07 15:54:22 -0500

    Adding example to print a number in binary format.

---
+++ dec2bin.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+def binary(n):
+   """Function to print binary number
+   for the input decimal using recursion"""
+   if n > 1:
+       binary(n//2)
+   print n%2,
+
+# Take decimal number from user
+dec = int(input("Enter an integer: "))
+binary(dec)
+
+
+
+
