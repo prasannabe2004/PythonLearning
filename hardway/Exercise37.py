11310c2f3fa9ac29fd8e60c981b77da21b86020c
prasannabe2004 <prasannabe2004@gmail.com>
2014-07-09 16:55:11 -0500

    and keyword tested.

--- hardway/Exercise37.py
+++ hardway/Exercise37.py
@@ -1,15 +1,22 @@
 #Python 2.7.0
 
+def test_and_keyword():
+	x = True
+	y = False
+	if x == True and y == False:
+		print "'and' keyword tested successfully"
+	else:
+		print "test failed"
 
 def test_del_keyword():
 	a = [1,2,3,4,5,6]
-	print a
 	del a[3]
-	print a
 	del a[2:4]
-	print a
+	print "'del' keyword tested successfully", a
 
 def start():
+	test_and_keyword()
+	print
 	test_del_keyword()
 
 start()
