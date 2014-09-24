d1727b69700fd9fe86ae9a33e0e512237c370933
prasannabe2004 <prasannabe2004@gmail.com>
2014-09-23 19:19:15 -0500

    codewars examples added.

--- hardway/Exercise37.py
+++ hardway/Exercise37.py
@@ -3,65 +3,75 @@
 
 from myfunctions2 import *
 
+
 def test_and_keyword():
-	x = True
-	y = False
-	if x == True and y == False:
-		print "'and' keyword tested successfully"
-	else:
-		print "test failed"
+    x = True
+    y = False
+    if x == True and y == False:
+        print "'and' keyword tested successfully"
+    else:
+        print "test failed"
+
 
 def test_del_keyword():
-	a = [1,2,3,4,5,6]
-	del a[3]
-	del a[2:4]
-	print "'del' keyword tested successfully", a
+    a = [1,2,3,4,5,6]
+    del a[3]
+    del a[2:4]
+    print "'del' keyword tested successfully", a
+
 
 def test_import_keyword():
-	import myfunctions
-	myfunctions.testfunction()
-	print "'import' keyword tested successfully"
+    import myfunctions
+    myfunctions.testfunction()
+    print "'import' keyword tested successfully"
+
 
 def test_from_keyword():
-	testfunction2()
-	print "'from' keyword tested successfully"
+    testfunction2()
+    print "'from' keyword tested successfully"
+
 
 def test_not_keyword():
-	x = True
-	print "'not' keyword tested successfully" , not x
+    x = True
+    print "'not' keyword tested successfully" , not x
+
 
 def test_while_keyword():
-	i = 1;
-	while(i<=2):
-		print i,
-		i = i+1
-	print "'while' keyword tested successfully"	
+    i = 1;
+    while(i<=2):
+        print i,
+        i = i+1
+    print "'while' keyword tested successfully"
+
 
 def test_if_elif_else_keywords():
-	user_input = raw_input("Are you a Python Programmer: ")
-	if (user_input == "Yes" or user_input == "yes" or user_input == "Y" or user_input == "y"):
-		print "I like you"
-	elif (user_input == "No" or user_input == "no" or user_input == "N" or user_input == "n"):
-		print "Why are you here"
-	else:
-		print "Invalid input"
+    user_input = raw_input("Are you a Python Programmer: ")
+    if (user_input == "Yes" or user_input == "yes" or user_input == "Y" or user_input == "y"):
+        print "I like you"
+    elif (user_input == "No" or user_input == "no" or user_input == "N" or user_input == "n"):
+        print "Why are you here"
+    else:
+    print "Invalid input"
+
 
 def test_global_keyword():
-	global value
-	value = 1000
+    global value
+    value = 1000
+
 
 def start():
-	"""
-	Test the documentation
-	"""
-	test_and_keyword()
-	test_del_keyword()
-	test_import_keyword()
-	test_from_keyword()
-	test_not_keyword()		
-	test_while_keyword()
-	test_if_elif_else_keywords()
-	test_global_keyword()
-	print value
-	
+    """
+    Test the documentation
+    """
+    test_and_keyword()
+    test_del_keyword()
+    test_import_keyword()
+    test_from_keyword()
+    test_not_keyword()
+    test_while_keyword()
+    test_if_elif_else_keywords()
+    test_global_keyword()
+    print value
+
+
 start()
