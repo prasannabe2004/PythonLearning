0c4493659d41e1f73896c07ef51848ae120e5bd8
prasannabe2004 <prasannabe2004@gmail.com>
2014-10-02 11:46:48 -0500

    Intro to CS from udacity exercises.

---
+++ scratchpad.py
@@ -0,0 +1,16 @@
+# Example 1
+marker = "AFK"
+replacement = "away from keyboard"
+line = "I will now go to sleep and be AFK until lunch time tomorrow."
+
+replaced = line.replace(marker,replacement)
+print replaced
+
+#Example 2 # uncomment this to test with different input
+marker = "EY"
+replacement = "Eyjafjallajokull"
+line = "The eruption of the volcano EY in 2010 disrupted air travel in Europe for 6 days."
+
+
+replaced = line.replace(marker,replacement)
+print replaced
