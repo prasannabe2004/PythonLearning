dff800a347a1467782455ba4121b4451f77ee6d7
prasannabe2004 <prasannabe2004@gmail.com>
2014-10-20 14:52:58 -0500

    Problem set 7  2 steps completed.

--- scratchpad.py
+++ scratchpad.py
@@ -1,8 +1,73 @@
-import re
+import string
 
-str = 'Hello, world!'
+class NewsStory(object):
+    def __init__(self, guid, title, subject, summary, link):
+        self.guid = guid
+        self.title = title
+        self.subject = subject
+        self.summary = summary
+        self.link = link
 
-list = str.split()
+    def getGuid(self):
+        return self.guid
+    def getTitle(self):
+        return self.title
+    def getSubject(self):
+        return self.subject
+    def getSummary(self):
+        return self.summary
+    def getLink(self):
+        return self.link
 
-print re.split(',|!|''',str)
+#======================
+# Part 2
+# Triggers
+#======================
+
+class Trigger(object):
+    def evaluate(self, story):
+        """
+        Returns True if an alert should be generated
+        for the given news item, or False otherwise.
+        """
+        raise NotImplementedError
+
+# Whole Word Triggers
+# Problems 2-5
+
+class WordTrigger(Trigger):
+    def __init__(self, word):
+        self.word = word
+
+    def isWordIn(self, text):
+        word = self.word.lower()
+        text = text.lower()
+        for i in string.punctuation:
+            if i in text:
+                text = text.replace(i, ' ')
+        text = text.split()
+
+        if word in text:
+            return True
+        else:
+            return False
+
+class TitleTrigger(WordTrigger):
+    def evaluate(self, story):
+        return self.isWordIn(story.getTitle())
+
+class SubjectTrigger(WordTrigger):
+    def evaluate(self, story):
+        return self.isWordIn(story.getSubject())
+
+class SummaryTrigger(WordTrigger):
+    def evaluate(self, story):
+        return self.isWordIn(story.getSummary())
+
+koala = NewsStory('', "Koala bear's are soft and cuddly", '', '', '')
+s1 = TitleTrigger('bear')
+s2 = TitleTrigger('soft')
+print s1.evaluate(koala)
+
+
 
