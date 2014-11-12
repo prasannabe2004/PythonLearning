0d91181065e01ccca52e38f43c5743c8e95d7d17
prasannabe2004 <prasannabe2004@gmail.com>
2014-11-11 23:30:13 -0600

    Removing the moved edx mooc.

--- edx/6.00.1.x/Week6/Lecture12/generator2.py
+++
@@ -1,6 +0,0 @@
-def genOneMillion():
-    maxNum = 1000000
-    current = -1
-    while current < maxNum:
-        current += 1
-        yield current
--- edx/6.00.1.x/iterativeMultiply.py
+++
@@ -1,9 +0,0 @@
-def iterativeMultiply(a,b):
-    result = 0
-    for i in range(b):
-        result = result + a
-    return result
-
-
-result = iterativeMultiply(50,6)
-print "a times b is: " , str(result)
