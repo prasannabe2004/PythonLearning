ef0d371626bcbf29b39de2dce8bab37e080087d9
prasannabe2004 <prasannabe2004@gmail.com>
2014-12-23 17:19:59 -0600

    fixing allignment issues.

--- divein/odbchelper.py
+++ divein/odbchelper.py
@@ -15,16 +15,15 @@ __copyright__ = "Copyright (c) 2001 Mark Pilgrim"
 __license__ = "Python"
 
 def buildConnectionString(params):
-	"""Build a connection string from a dictionary
-	
-	Returns string.
-	"""
-	return ";".join(["%s=%s" % (k, v) for k, v in params.items()])
+    """Build a connection string from a dictionary
+    Returns string.
+    """
+    return ";".join(["%s=%s" % (k, v) for k, v in params.items()])
 
 if __name__ == "__main__":
-	myParams = {"server":"mpilgrim", \
-				"database":"master", \
-				"uid":"sa", \
-				"pwd":"secret"
-				}
-	print buildConnectionString(myParams)
+    myParams = {"server": "mpilgrim",
+                "database": "master",
+                "uid": "sa",
+                "pwd": "secret"
+                }
+    print buildConnectionString(myParams)
