e761f9d6806d851d9e040c56a38455f26d8f73d9
prasannabe2004 <prasannabe2004@gmail.com>
2016-02-07 17:03:50 -0600

    numpy examples

--- edX/DAT208x/testNumpy.py
+++ edX/DAT208x/testNumpy.py
@@ -1,7 +1,13 @@
+# Create list baseball 
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Import the numpy package as np
 import numpy as np
 
-myarray = np.array([1, 2, 3, 4])
+# Create a Numpy array from baseball: np_baseball
+np_baseball=np.array(baseball)
 
-print(myarray)
+# Print out type of np_baseball
+print(type(np_baseball))
 
-
+print(np_baseball)
